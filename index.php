ef95ea79d6f7dc0e4c4c936408cadfff4e31b572
Lemonisn <164480574+Lemonisn@users.noreply.github.com>
2025-06-02 19:28:25 -0700

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   session_start();
   require_once 'header.php';
 
-  echo "<div class='center'>Welcome to Robin's Nest,";
+  echo "<div class='center'>Welcome to Webwizard,";
 
   if ($loggedin) echo " $user, you are logged in";
   else           echo ' please sign up or log in';
@@ -12,7 +12,7 @@
     </div>
     <div data-role="footer">
       <h4>Web App from <i><a href='https://lemonisn.github.io/webwizard-demo/'
-      target='_blank'>Learning PHP MySQL & JavaScript</a></i></h4>
+      target='_blank'>Login to start.</a></i></h4>
     </div>
   </body>
 </html>

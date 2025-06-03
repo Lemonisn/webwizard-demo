af8bb99ddf8528fdacaf9f2f683b98dff3fa8596
Lemonisn <164480574+Lemonisn@users.noreply.github.com>
2025-06-02 20:41:00 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Example 14: javascript.js
+// 
 
 function O(i) { return typeof i == 'object' ? i : document.getElementById(i) }
 function S(i) { return O(i).style                                            }
